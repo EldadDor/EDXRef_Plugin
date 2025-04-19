2976b438634cc2902cb8bb398473d75f8e77b1ca
EldadDor <eldaddev@gmail.com>
2025-04-19 21:30:48 +0300

    Fixing bug with saving settings

--- src/main/kotlin/com/github/edxref/settings/WSConsumerSettingsConfigurable.kt
+++ src/main/kotlin/com/github/edxref/settings/WSConsumerSettingsConfigurable.kt
@@ -54,7 +54,9 @@ class WSConsumerSettingsConfigurable : Configurable {
         settingsComponent!!.wsParamAnnotationFqn = settings.wsParamAnnotationFqn
         settingsComponent!!.propertyAnnotationFqn = settings.propertyAnnotationFqn
         settingsComponent!!.validatePropertyAnnotations = settings.validatePropertyAnnotations
-        settingsComponent!!.httpRequestAnnotationFqn = settings.httpRequestAnnotationFqn // New setting
+        settingsComponent!!.httpRequestAnnotationFqn = settings.httpRequestAnnotationFqn
+        settingsComponent!!.wsHeaderAnnotationFqn = settings.wsHeaderAnnotationFqn
+        settingsComponent!!.wsHeadersAnnotationFqn = settings.wsHeadersAnnotationFqn
     }
 
 
