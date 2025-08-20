136440be6e2665d834fc58c8da48ceaff66f4ca1
eldad <eldad@relay.yashir.co.il>
2025-08-20 17:56:14 +0300

    Trying to fix index loading problem

--- src/main/kotlin/com/github/edxref/query/index/QueryUtilsUsageIndex.kt
+++ src/main/kotlin/com/github/edxref/query/index/QueryUtilsUsageIndex.kt
@@ -3,6 +3,7 @@ package com.github.edxref.query.index
 import com.github.edxref.query.settings.QueryRefSettings.Companion.getQueryRefSettings
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.psi.* // Import base PsiFile
 import com.intellij.util.indexing.*
@@ -11,77 +12,81 @@ import com.intellij.util.io.KeyDescriptor
 
 class QueryUtilsUsageIndex : FileBasedIndexExtension<String, String>() {
 
-  private fun getSettings(project: Project) = project.getQueryRefSettings()
+	private fun getSettings(project: Project) = project.getQueryRefSettings()
 
-  private fun getQueryUtilsFqn(project: Project) =
-    getSettings(project).queryUtilsFqn.ifBlank { "com.example.QueryUtils" }
+	private fun getQueryUtilsFqn(project: Project) =
+		getSettings(project).queryUtilsFqn.ifBlank { "com.example.QueryUtils" }
 
-  private fun getQueryUtilsMethodName(project: Project) =
-    getSettings(project).queryUtilsMethodName.ifBlank { "getQuery" }
+	private fun getQueryUtilsMethodName(project: Project) =
+		getSettings(project).queryUtilsMethodName.ifBlank { "getQuery" }
 
-  companion object {
-    val KEY: ID<String, String> = ID.create("com.edxref.queryutils.usage.index")
-    private val log: Logger = logger<QueryUtilsUsageIndex>()
-    // Consider adding expected method/qualifier names here if they are relatively fixed
-    // or accept that the index might contain slightly more than needed, to be filtered later.
-    // Example
-  }
+	companion object {
+		val KEY: ID<String, String> = ID.create("com.edxref.queryutils.usage.index")
+		private val log: Logger = logger<QueryUtilsUsageIndex>()
+		// Consider adding expected method/qualifier names here if they are relatively fixed
+		// or accept that the index might contain slightly more than needed, to be filtered later.
+		// Example
+	}
 
-  override fun getName(): ID<String, String> = KEY
+	override fun getName(): ID<String, String> = KEY
 
-  override fun getKeyDescriptor(): KeyDescriptor<String> = EnumeratorStringDescriptor.INSTANCE
+	override fun getKeyDescriptor(): KeyDescriptor<String> = EnumeratorStringDescriptor.INSTANCE
 
-  override fun getValueExternalizer() = EnumeratorStringDescriptor.INSTANCE
+	override fun getValueExternalizer() = EnumeratorStringDescriptor.INSTANCE
 
-  override fun getVersion(): Int = 2 // Increment version due to logic change
+	override fun getVersion(): Int = 2 // Increment version due to logic change
 
-  override fun dependsOnFileContent(): Boolean = true
+	override fun dependsOnFileContent(): Boolean = true
 
-  override fun getInputFilter(): FileBasedIndex.InputFilter {
-    // Index only Java files
-    return DefaultFileTypeSpecificInputFilter(com.intellij.ide.highlighter.JavaFileType.INSTANCE)
-  }
+	override fun getInputFilter(): FileBasedIndex.InputFilter {
+		// Index only Java files
+		return DefaultFileTypeSpecificInputFilter(com.intellij.ide.highlighter.JavaFileType.INSTANCE)
+	}
 
-  override fun getIndexer(): DataIndexer<String, String, FileContent> {
-    return DataIndexer { inputData ->
-      val map = mutableMapOf<String, String>()
-      val psiFile = inputData.psiFile
-      // Use a visitor to find relevant elements efficiently
-      psiFile.accept(
-        object : JavaRecursiveElementVisitor() {
-          override fun visitMethodCallExpression(call: PsiMethodCallExpression) {
-            super.visitMethodCallExpression(call) // Continue visiting children
+	override fun getIndexer(): DataIndexer<String, String, FileContent> {
+		return DataIndexer { inputData ->
+			val map = mutableMapOf<String, String>()
+			val psiFile = inputData.psiFile
+			val project = psiFile.project
 
-            val methodExpr = call.methodExpression
-            // Simple check for method name (adjust if needed from settings later)
-            if (methodExpr.referenceName == getQueryUtilsMethodName(psiFile.project)) {
-              // Simple check for qualifier text (less reliable but indexer-safe)
-              val qualifierText = methodExpr.qualifierExpression?.text
+			// Check dumb mode to avoid IndexNotReadyException
+			val (queryUtilsFqn, queryUtilsMethodName) = if (DumbService.isDumb(project)) {
+				// Use defaults during dumb mode
+				"com.example.QueryUtils" to "getQuery"
+			} else {
+				// Use settings when project is ready
+				val settings = project.getQueryRefSettings()
+				val fqn = settings.queryUtilsFqn.ifBlank { "com.example.QueryUtils" }
+				val method = settings.queryUtilsMethodName.ifBlank { "getQuery" }
+				fqn to method
+			}
+			psiFile.accept(
+				object : JavaRecursiveElementVisitor() {
+					override fun visitMethodCallExpression(call: PsiMethodCallExpression) {
+						super.visitMethodCallExpression(call)
 
-              if (
-                qualifierText == "queryUtils" || qualifierText == getQueryUtilsFqn(psiFile.project)
-              ) { // Example check
+						val methodExpr = call.methodExpression
+						if (methodExpr.referenceName == queryUtilsMethodName) {
+							val qualifierText = methodExpr.qualifierExpression?.text
 
-                // Get the argument (assuming it's the first argument and a literal)
-                val args = call.argumentList.expressions
-                if (args.size == 1 && args[0] is PsiLiteralExpression) {
-                  val literal = args[0] as PsiLiteralExpression
-                  val queryId = literal.value as? String
-                  if (!queryId.isNullOrBlank()) {
-                    // Index the queryId -> filePath
-                    // We don't perform the full FQN check here!
-                    log.debug(
-                      "Indexer found potential usage: ID='$queryId' in ${inputData.file.name}"
-                    )
-                    map[queryId] = inputData.file.path
-                  }
-                }
-              } // End qualifier check
-            }
-          }
-        }
-      )
-      map
-    }
-  }
+							if (qualifierText == "queryUtils" || qualifierText == queryUtilsFqn) {
+								val args = call.argumentList.expressions
+								if (args.size == 1 && args[0] is PsiLiteralExpression) {
+									val literal = args[0] as PsiLiteralExpression
+									val queryId = literal.value as? String
+									if (!queryId.isNullOrBlank()) {
+										log.debug(
+											"Indexer found potential usage: ID='$queryId' in ${inputData.file.name}"
+										)
+										map[queryId] = inputData.file.path
+									}
+								}
+							}
+						}
+					}
+				}
+			)
+			map
+		}
+	}
 }
--- src/main/kotlin/com/github/edxref/query/index/SQLRefAnnotationIndex.kt
+++ src/main/kotlin/com/github/edxref/query/index/SQLRefAnnotationIndex.kt
@@ -11,6 +11,7 @@ package com.github.edxref.query.index
 import com.github.edxref.query.settings.QueryRefSettings.Companion.getQueryRefSettings
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.diagnostic.logger
+import com.intellij.openapi.project.DumbService
 import com.intellij.psi.JavaRecursiveElementVisitor
 import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiJavaFile
@@ -20,61 +21,66 @@ import com.intellij.util.io.KeyDescriptor
 
 class SQLRefAnnotationIndex : FileBasedIndexExtension<String, String>() {
 
-  companion object {
-    val KEY: ID<String, String> = ID.create("com.edxref.sqlref.annotation.index")
-    private val log: Logger = logger<SQLRefAnnotationIndex>()
-  }
+	companion object {
+		val KEY: ID<String, String> = ID.create("com.edxref.sqlref.annotation.index")
+		private val log: Logger = logger<SQLRefAnnotationIndex>()
+	}
 
-  override fun getName(): ID<String, String> = KEY
+	override fun getName(): ID<String, String> = KEY
 
-  override fun getKeyDescriptor(): KeyDescriptor<String> = EnumeratorStringDescriptor.INSTANCE
+	override fun getKeyDescriptor(): KeyDescriptor<String> = EnumeratorStringDescriptor.INSTANCE
 
-  override fun getValueExternalizer() = EnumeratorStringDescriptor.INSTANCE
+	override fun getValueExternalizer() = EnumeratorStringDescriptor.INSTANCE
 
-  override fun getVersion(): Int = 1
+	override fun getVersion(): Int = 1
 
-  override fun dependsOnFileContent(): Boolean = true
+	override fun dependsOnFileContent(): Boolean = true
 
-  override fun getInputFilter(): FileBasedIndex.InputFilter {
-    // Index only Java files
-    return DefaultFileTypeSpecificInputFilter(com.intellij.ide.highlighter.JavaFileType.INSTANCE)
-  }
+	override fun getInputFilter(): FileBasedIndex.InputFilter {
+		// Index only Java files
+		return DefaultFileTypeSpecificInputFilter(com.intellij.ide.highlighter.JavaFileType.INSTANCE)
+	}
 
-  override fun getIndexer(): DataIndexer<String, String, FileContent> {
-    return DataIndexer { inputData ->
-      val map = mutableMapOf<String, String>()
-      val psiFile = inputData.psiFile
+	override fun getIndexer(): DataIndexer<String, String, FileContent> {
+		return DataIndexer { inputData ->
+			val map = mutableMapOf<String, String>()
+			val psiFile = inputData.psiFile
 
-      if (psiFile is PsiJavaFile) {
-        val project = psiFile.project
-        val settings = project.getQueryRefSettings()
-        val annotationFqn = settings.sqlRefAnnotationFqn.ifBlank { "com.github.edxref.SQLRef" }
-        val attributeName = settings.sqlRefAnnotationAttributeName.ifBlank { "refId" }
+			if (psiFile is PsiJavaFile) {
+				val project = psiFile.project
 
-        psiFile.accept(
-          object : JavaRecursiveElementVisitor() {
-            override fun visitAnnotation(annotation: PsiAnnotation) {
-              super.visitAnnotation(annotation)
+				// Check if project is in dumb mode to avoid IndexNotReadyException
+				val (annotationFqn, attributeName) = if (DumbService.isDumb(project)) {
+					// Use defaults during dumb mode
+					"com.github.edxref.SQLRef" to "refId"
+				} else {
+					// Use settings when project is ready
+					val settings = project.getQueryRefSettings()
+					val fqn = settings.sqlRefAnnotationFqn.ifBlank { "com.github.edxref.SQLRef" }
+					val attr = settings.sqlRefAnnotationAttributeName.ifBlank { "refId" }
+					fqn to attr
+				}
+				psiFile.accept(
+					object : JavaRecursiveElementVisitor() {
+						override fun visitAnnotation(annotation: PsiAnnotation) {
+							super.visitAnnotation(annotation)
 
-              // Check if this is an SQLRef annotation
-              if (annotation.qualifiedName == annotationFqn) {
-                val refIdValue =
-                  annotation.findAttributeValue(attributeName)?.text?.replace("\"", "")
+							if (annotation.qualifiedName == annotationFqn) {
+								val refIdValue =
+									annotation.findAttributeValue(attributeName)?.text?.replace("\"", "")
 
-                if (!refIdValue.isNullOrBlank()) {
-                  // Index the refId -> filePath
-                  log.debug(
-                    "Indexer found SQLRef annotation: refId='$refIdValue' in ${inputData.file.name}"
-                  )
-                  map[refIdValue] = inputData.file.path
-                }
-              }
-            }
-          }
-        )
-      }
-
-      map
-    }
-  }
+								if (!refIdValue.isNullOrBlank()) {
+									log.debug(
+										"Indexer found SQLRef annotation: refId='$refIdValue' in ${inputData.file.name}"
+									)
+									map[refIdValue] = inputData.file.path
+								}
+							}
+						}
+					}
+				)
+			}
+			map
+		}
+	}
 }
